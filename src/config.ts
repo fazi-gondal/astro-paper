fb07366003c453e4f67222deda3121ae4e301b29
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2024-05-28 07:38:34 +0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Code With Fazi",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
